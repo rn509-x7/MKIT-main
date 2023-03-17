96de707177fa0d882376a3e2f730a5ec7008e94e
RN 509 - X7 <127879219+rn509@users.noreply.github.com>
2023-03-17 16:52:00 +0700

    Update t1.py

--- tools/t1.py
+++ tools/t1.py
@@ -4,11 +4,11 @@ import time
 
 print("""
 | ======================================= |
-|  https://github.com/MsecTeam/MsecEmpas  |
+|  https://github.com/rn509/AccountEmpas-main-  |
 | ======================================= |
 
 Running..
      """)
 time.sleep(3)
 os.system("apt install toilet")
-os.system("python main/MsecEmpas/empas.py")
+os.system("python main/AccountEmpas-main-/empas.py")
